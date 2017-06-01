a3bb3c7fbc0c536a8833951c4fc8255b0de31201
Jayson Bailey <avoidiam@gmail.com>
2017-06-01 12:29:06 -0600

    fixed ppg and ppip

--- ottoneu_notsuck.js
+++ ottoneu_notsuck.js
@@ -4,11 +4,11 @@ $(hittersTable).attr("id", "hittersTable");
 $(pitchersTable).attr("id", "pitchersTable");
 
 // Add P/G to hitting table
-$(hittersTable).find("tr").eq(0).find("th").eq(1).attr('colspan', 13);
+$(hittersTable).find("tr").eq(0).find("th").eq(1).attr('colspan', 14);
 $(hittersTable).find("tr.statHeaders").eq(0).append('<th class="notRounded">P/G</th>');
 
 // Add P/IP to pitching table
-$(pitchersTable).find("tr").eq(0).find("th").eq(1).attr('colspan', 12);
+$(pitchersTable).find("tr").eq(0).find("th").eq(1).attr('colspan', 13);
 $(pitchersTable).find(".statHeaders").eq(0).append('<th class="notRounded">P/IP</th>');
 
 function round2(num) {
@@ -17,10 +17,12 @@ function round2(num) {
 
 function calcPPG(row) {
   strGames = $(row).find("td").eq(5).html();
-  strPoints = $(row).find("td").eq(15).html();
+  strPoints = $(row).find("td").eq(14).html();
+
   if(strPoints != null && strPoints.indexOf(',') != -1) {
     strPoints = strPoints.replace(/\,/,'');
   }
+
   fGames = parseFloat(strGames);
   fPoints = parseFloat(strPoints);
 
@@ -31,16 +33,19 @@ function calcPPG(row) {
 }
 
 function calcPPIP(row) {
-  strInnings = $(row).find("td").eq(5).html();
-  strPoints = $(row).find("td").eq(13).html();
+  strInnings = $(row).find("td").eq(4).html();
+  strPoints = $(row).find("td").eq(12).html();
+
   if(strPoints != null && strPoints.indexOf(',') != -1) {
     strPoints = strPoints.replace(/\,/,'');
   }
+
   fInnings = parseFloat(strInnings);
   fPoints = parseFloat(strPoints);
 
   var partial = Math.round((fInnings - Math.floor(fInnings)) * 100) / 100;
   fInnings = Math.floor(fInnings);
+
   if(partial == 0.1) {
     fInnings += 0.33;
   } else if(partial == 0.2) {
